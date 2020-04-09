0c805722d861d34c322ba481f29ff92fe3910b91
Maria Saenz Reina <mariasaenzreina2016@gmail.com>
2020-04-09 14:43:59 -0600

    Se Finaliza el Formulario de Login

--- JS/app.js
+++ JS/app.js
@@ -89,4 +89,25 @@ abajo.addEventListener('click', () => {
   document.documentElement.scrollTop = 600;
   
 });
+// ************Formulario Login**************
+const inputs = document.querySelectorAll(".input");
 
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+//********Fin Formulario Login*************/
